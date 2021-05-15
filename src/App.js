624970a5b1fdd62919bb0cd4578db15e8c0b1573
Joy <utsho233@gmail.com>
2021-05-15 18:20:46 +0600

    finished Project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 // import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList'
+import './App.css'
+
 
 function App() {
   return (
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,20 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect, useRef} from 'react'
 
-const TodoForm = (props) => {
-    const [input, setInput] = useState("")
+function TodoForm (props)  {
+    const [input, setInput] = useState(props.edit ? props.edit.value : '')
+
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
+
+
+
+
+
+
+
 
     const handleChange = e =>{
         setInput(e.target.value)
@@ -24,9 +37,38 @@ const TodoForm = (props) => {
 
 
     return (
-        <form className="todo-form" onSubmit={handleSubmit}> 
-            <input type="text"placeholder="add a todo" value={input} name="text" className="todo-input" onChange={handleChange} />
+        <form className="todo-form" onSubmit={handleSubmit}>
+            {props.edit ? (
+            <>
+            <input 
+                type="text"
+                placeholder="Update Your Item" 
+                value={input} 
+                name="text" 
+                className="todo-input edit" 
+                onChange={handleChange}
+                ref={inputRef} 
+            />
+            <button className="todo-button edit">Update</button> 
+            </>
+            
+            ) :
+            (
+               <> 
+                <input 
+                type="text"
+                placeholder="add a todo" 
+                value={input} 
+                name="text" 
+                className="todo-input" 
+                onChange={handleChange}
+                ref={inputRef} 
+            />
             <button className="todo-button">Add Todo</button>
+            </>
+            
+            ) } 
+            
         </form>
     )
 }
